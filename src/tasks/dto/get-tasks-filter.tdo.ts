9272eb6e2e5c7f6b34582c5c7e99e597db530f82
André Wronscki <andre.wronscki@totalvoice.com.br>
2020-03-04 17:44:41 -0300

    Persistindo dados no banco de dados Postgres

--- src/tasks/dto/get-tasks-filter.tdo.ts
+++ src/tasks/dto/get-tasks-filter.tdo.ts
@@ -1,7 +1,7 @@
-import { TaskStatus } from '../tasks.model';
+import { TaskStatus } from '../task-status.enum';
 import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
-export class GetTasksFIlterDto {
+export class GetTasksFilterDto {
   @IsOptional()
   @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status: TaskStatus;
